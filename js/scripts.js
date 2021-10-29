5d7e57d85196406a9be14c0e69104cd831ca01ec
Katie Pundt <kzpundt@gmail.com>
2021-10-29 13:03:55 -0700

    commented out functions used for testing

--- js/scripts.js
+++ js/scripts.js
@@ -29,27 +29,26 @@ function mrRoboger(input) {
 
 // UI logic
 
-function singleDigit(input) {
-  if (input === 1) {
-    return "Beep!";
-  } else if (input === 2) {
-    return "Boop!";
-  } else if (input === 3) {
-    return "Won't you be my neighbor?";
-  } else {
-    return false;
-  }
-}
+// function singleDigit(input) {
+//   if (input === 1) {
+//     return "Beep!";
+//   } else if (input === 2) {
+//     return "Boop!";
+//   } else if (input === 3) {
+//     return "Won't you be my neighbor?";
+//   } else {
+//     return false;
+//   }
+// }
 
-
-function multiDigit(input) {
-  if (input.includes("3")) {
-    return "Won't you be my neighbor?";
-  } else if (input.includes("2")) {
-    return "Boop!";
-  } else if (input.includes("1")) {
-    return "Beep!";
-  } else {
-    return false;
-  }
-}
+// function multiDigit(input) {
+//   if (input.includes("3")) {
+//     return "Won't you be my neighbor?";
+//   } else if (input.includes("2")) {
+//     return "Boop!";
+//   } else if (input.includes("1")) {
+//     return "Beep!";
+//   } else {
+//     return false;
+//   }
+// }
